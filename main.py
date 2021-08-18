6327f4cc327418e22d33ddd0198b1b9d4771c35a
shss2603 <forshashankshekhar@gmail.com>
2021-08-18 18:26:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 from typing import List
 import base64
 import os
+import json
 class Urls(BaseModel):
     urls : List[str]
 
@@ -21,9 +22,19 @@ async def create_item(url_list: Urls):
         if not (os.path.isdir(url_64)):
             print("inside if")
             encoded_url_uncached.append(url_64)
-
-
     encoded_url_str = ",".join(encoded_url_uncached)
     if not (encoded_url_uncached == []):
         subprocess.run(["node","example.js",encoded_url_str])
-    return encoded_url
+    current_path = os.getcwd()
+    result_json = {}
+    i = 0
+    for url_64 in encoded_url:
+        search_path = current_path + "/"+url_64
+        os.chdir(search_path)
+        f = open('inspection.json')
+        data = json.load(f)
+        result_json[url_list.urls[i]] = data
+        f.close()
+        i = i+1
+    os.chdir(current_path)
+    return result_json
