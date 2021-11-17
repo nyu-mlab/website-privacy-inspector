c4d300432f61cad034af6f76077f7aa5666e6849
shss2603 <forshashankshekhar@gmail.com>
2021-11-17 14:32:51 -0500

    Update example.js

--- example.js
+++ example.js
@@ -3,24 +3,20 @@ const { join } = require("path");
 const fs = require('fs');
 (async () => {
   const EMULATE_DEVICE = "";
-
   // Save the results to a folder
   let OUT_DIR = true;
-
-  // The URL to test
   const URL = process.argv[2];
   var log = process.argv[3];
-  console.log(log);
-
+  const hashes = process.argv[4];
+  console.log(hashes)
   var url_list = URL.split(",");
-  //const url1 = string[0];
-  //const url2 = string[1];
+  var url_hash_list = hashes.split(",");
   var today = new Date();
   var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
   var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
   var dateTime = date+' '+time;
   log = log + dateTime  + " Decoding of encoded URL started \n";
-
+  console.log(log);
   for(let i =0;i<url_list.length;i++)
 {
     date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
@@ -33,6 +29,7 @@ const fs = require('fs');
     time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
     dateTime = date+ " " +time;
     log = log + dateTime + " " + url_base64 + " decoded to " + url + "\n";
+    console.log(dateTime + " " + url_base64 + " decoded to " + url + "\n")
     var defaultConfig = {
     inUrl: url, //remove https
     numPages: 0,
@@ -40,15 +37,17 @@ const fs = require('fs');
     emulateDevice: EMULATE_DEVICE,
   };
     fs.writeFileSync('logfile.txt',log);
-    var destination_dict = url_list[i]
-        var result = await collector(
+    var destination_folder = url_hash_list[i]
+	var result = await collector(
     OUT_DIR
-      ? { ...defaultConfig, ...{ outDir: join(__dirname, destination_dict) } }
+      ? { ...defaultConfig, ...{ outDir: join(__dirname, destination_folder) } }
       : defaultConfig
   );
   if (OUT_DIR) {
     console.log(
-      `For captured data please look in ${join(__dirname, destination_dict)}`
+      `For captured data please look in ${join(__dirname, destination_folder)}`
     );
   }
 }
+})();
+
